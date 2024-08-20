f0bdc91c0ed3fb5d51976eb23a8a584bde3e4d0f
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2024-08-20 12:18:31 +0900

    sky-wingsのサイトをReactでの作成を開始します。

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Hello World!</h1>
+                <p>description.</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
